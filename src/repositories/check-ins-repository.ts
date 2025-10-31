7752eeb83576db0d32de2dec0184246c00fd2777
Fernando Melo <fldm.sc@gmail.com>
2025-10-31 16:28:07 -0300

    estudo 311025
    estudo

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -3,6 +3,7 @@ import { Checkin, Prisma } from "@prisma/client";
 export interface CheckInsRepository{
     findByUserIdOnDate(userId:String, date: Date): Promise<Checkin | null>
     findManyByUserId(userID:String, page:number): Promise<Checkin[]>
+    countByUserId(userId: String): Promise<number>
     create(data: Prisma.CheckinUncheckedCreateInput) : Promise<Checkin>
 
 }
